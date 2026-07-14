fe44628e85cf64284f28f996ab706f18fe9104ce
tiya2031 <tiyaaa3120@gmail.com>
2026-07-14 19:55:10 +0530

    first commit

---
+++ js-main/01_basic/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict";//treat code as new version
+
+// alert(3 + 3) we are using node.js not browser
+
+console.log(3 + 3);
+
+console.log("babyy");
+
+let name = "riya"
+let age = 19
+let isLoggedIn = false 
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
+// num 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null standalone value
+// undefined
+// symbol unique
+// object
+
+
---
+++ js-main/01_basic/test.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "rr3120@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "r31@google.com"
+accountPassword = "4435444"
+accountCity = "mumbai"
+
+
+// dont use var//
+console.log(accountId);
+console.table([accountId, accountPassword, accountEmail, accountCity])
