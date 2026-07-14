735d552b0d2c5e1a6b1c1874c7853c683039c0fe
tiya2031 <tiyaaa3120@gmail.com>
2026-07-14 21:35:33 +0530

    convop

---
+++ js-main/01_basic/03_convop.js
@@ -0,0 +1,23 @@
+let score = "r"
+// console.log(typeof (score));
+// console.log(typeof score);
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+// "33" 33
+// "33abs" nan
+// true 1 false 0
+let isLoggedIn = ""
+let booleanIsloggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsloggedIn);
+// "" false
+// r true
+let somenumber = 31
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
