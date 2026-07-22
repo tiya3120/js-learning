cd8d8e653ed133a1673584e92408df371b7c01c3
tiya2031 <tiyaaa3120@gmail.com>
2026-07-22 16:36:43 +0530

    nummaths

---
+++ js-main/01_basic/06_nummaths.js
@@ -0,0 +1,28 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+const othernamber = 31.43874
+console.log(othernamber.toPrecision(3));
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-In'));
+// maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,2,5,4,7,8));
+// console.log(Math.max(4,2,1,7,5,));
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
