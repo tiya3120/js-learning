09786428086545af8eaaaaca55f230ac42a822fd
tiya2031 <tiyaaa3120@gmail.com>
2026-07-23 16:19:59 +0530

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,29 @@
+// array
+const myarray = [0,1,2,3,4]
+// const myheros =["virat","rohit"]
+// const myarr2 = new Array(1,2,3)
+// console.log(myarray[5]);
+// array methods
+
+// myarray.push(5)
+// myarray.pop()
+// myarray.unshift(3)
+// myarray.shift()
+// console.log(myarray.includes(9));
+// console.log(myarray.indexOf(9));
+
+// const newarray = myarray.join()
+// console.log(myarray);
+// console.log(newarray);
+
+// slice,splice
+
+console.log("a",myarray);
+const myn1 = myarray.slice(1,3)
+console.log(myn1);
+console.log("b",myarray);
+const myn2 = myarray.splice(1,3)
+console.log("c",myarray);
+
+console.log(myn2);
+
